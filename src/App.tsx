d03704cac542c5fe64e460fd8cb20601b7879c57
Anju Bala <anju@klavenessdigital.com>
2022-12-16 21:25:03 +0100

    fetch json and also pass header to tell the client to accept json from server

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
+import React, { useEffect } from "react";
 import "./App.css";
 
 function App() {
+    const getData = () => {
+        fetch("parking-garage.json", {
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+        })
+            .then(function (response) {
+                console.log(response);
+                return response.json();
+            })
+            .then(function (myJson) {
+                console.log(myJson);
+            });
+    };
+    useEffect(() => {
+        getData();
+    }, []);
+
     return (
         <div className="App">
             <div>Hi</div>
